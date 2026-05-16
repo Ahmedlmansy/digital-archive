a6dc655ec5b494299e6567d68289ef0852bc0b80
Ahmed <ahmedelmansy579@gmail.com>
2026-05-16 12:35:54 +0300

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 
 import './style/App.css'
 import AppRouter from './routes/AppRouter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { checkAuth } from './features/auth/authSlice';
 
 function App() {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, []);
   return <AppRouter />;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from 'store'
+import { store } from "@/features/store.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
